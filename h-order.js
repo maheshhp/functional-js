ac4a07b9fc39fb9f1f251e95f9a342a916665381
maheshhp <hpmahesh73@gmail.com>
2018-01-16 11:34:32 +0530

    Exercise two: operation running required number of times passing

--- h-order.js
+++ h-order.js
@@ -1,7 +1,9 @@
 let counter = 0;
 function repeat(operation, num) {
       // SOLUTION GOES HERE
-	
+      for(let i=0; i<num; i++){
+	operation();
+      }
     }
 
     // Do not remove the line below
@@ -14,3 +16,4 @@ function testRepeat(fn, times){
 }
 
 console.log("Function not run when times is 0 : ", testRepeat(() => {counter+=1;}, 0) === 0);
+console.log("Function running required number of times : ", testRepeat(() => {counter+=1;}, 5) === 5);
