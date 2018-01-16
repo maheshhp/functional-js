0e44dc40985c9577be7b2ee1cdb40d2fc18a61ef
maheshhp <hpmahesh73@gmail.com>
2018-01-16 11:23:23 +0530

    Exercise two: Zero times case passing

--- h-order.js
+++ h-order.js
@@ -1,6 +1,16 @@
+let counter = 0;
 function repeat(operation, num) {
       // SOLUTION GOES HERE
+	
     }
 
     // Do not remove the line below
-module.exports = repeat
+//module.exports = repeat
+
+function testRepeat(fn, times){
+   counter = 0; 
+   repeat(fn, times);
+   return counter;
+}
+
+console.log("Function not run when times is 0 : ", testRepeat(() => {counter+=1;}, 0) === 0);
