3957bddb77dbcf0a4262ae0d545018b692371133
maheshhp <hpmahesh73@gmail.com>
2018-01-16 14:49:00 +0530

    Return correct number of messages test passing

---
+++ filter-msg.js
@@ -0,0 +1,16 @@
+function getShortMessages(messages) {
+  let shortMsg = messages.filter(a => {if(a.message.length < 50) return true; return false;}).map(a => a.message);
+  return shortMsg;
+}
+
+function verifyGetMsg(messages, num) {
+  let returnedMsg = getShortMessages(messages);
+  if (returnedMsg.length === num) {
+    return true;
+  }
+  return false;
+}
+
+console.log("Returning messages less than 50 characters : ", verifyGetMsg([{message: 'jhadfgHJADFGJHAFGHJGFHJGFHJGFAJGJHGghgfjhgdfjhgdhgfjhgdfhjgdhgdhhfgfsghjsghjgfhjgfhjghdsfjdgfjhsgdfhjgfjhgdgfjhdsgfdsfgfjdgfjdgfj'}, {message: 'abcdefg'}], 1));
+
+// module.exports = getShortMessages
