4b1983828b50a5c86bdc338163663f3dd7aadc82
maheshhp <hpmahesh73@gmail.com>
2018-01-16 10:15:21 +0530

    First exercise: Upper case return passing

--- hello-world.js
+++ hello-world.js
@@ -1,8 +1,8 @@
 function upperCaser(input) {
-      return input;
+      return input.toUpperCase();
     }
 //Checking for return value in same case
-console.log("Checking return in same case", upperCaser("Hello World") === "Hello World");
-
+//console.log("Checking return in same case", upperCaser("Hello World") === "Hello World");
+console.log("Checking return in upper case", upperCaser("Hello World") === "HELLO WORLD");
 //module.exports = upperCaser
 
