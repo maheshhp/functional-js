3ebcf5daa2968530d3c54c7341207d2247da78d8
maheshhp <hpmahesh73@gmail.com>
2018-01-17 21:40:34 +0530

    Exercise nine: Added test scenarios

---
+++ w-bind.js
@@ -0,0 +1,14 @@
+let slice = Array.prototype.slice;
+
+function logger(namespace) {
+  // SOLUTION GOES HERE
+}
+
+
+//Tests for logger function --> not implemented
+console.log("Prints prepend string when message is empty");
+
+console.log("Prints complete message string with prepend when message is not empty");
+
+console.log("No output on screen when both message and prepend string are empty");
+module.exports = logger;
