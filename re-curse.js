2077381bb4d92d9f8c4752e6ff734e9c9c9a53da
maheshhp <hpmahesh73@gmail.com>
2018-01-17 12:53:41 +0530

    Exercise seven: Added tests for add function - passing

---
+++ re-curse.js
@@ -0,0 +1,25 @@
+
+function addValues(pre, next) {
+  if (pre === null || next === null) {
+    return 0;
+  }
+  return pre + next;
+}
+
+function reduce(arr, fn, initial) {
+  // SOLUTION GOES HERE
+
+}
+let inputWords = [1, 2, 3, 4];
+let noInputWords = [];
+
+//Testing reduce function
+// console.log("Return [] for zero objects : ", reduce(noInputWords, addValues, 0) === 0);
+// console.log("Return correct count values : ", reduce(inputWords, addValues, 0) === 10);
+
+//Testing addValues function
+console.log("Return 0 for 0 as input : ", addValues(0, 0) === 0);
+console.log("Return 0 for null as first input : ", addValues(null, 0) === 0);
+console.log("Return 0 for null as second input : ", addValues(0, null) === 0);
+console.log("Return 5 for 2 and 3 as inputs : ", addValues(2, 3) === 5);
+module.exports = reduce;
