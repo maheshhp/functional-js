999e2c96ae82435dab6880d0a0dc479fd4707996
maheshhp <hpmahesh73@gmail.com>
2018-01-17 21:43:42 +0530

    Exercise nine: Verified exercise

--- w-bind.js
+++ w-bind.js
@@ -2,13 +2,17 @@ let slice = Array.prototype.slice;
 
 function logger(namespace) {
   // SOLUTION GOES HERE
+  return function() {
+    let slicedArgs = slice.call(arguments);
+    console.log.apply(null, [namespace].concat(slicedArgs));
+  };
 }
 
 
 //Tests for logger function --> not implemented
-console.log("Prints prepend string when message is empty");
-
-console.log("Prints complete message string with prepend when message is not empty");
-
-console.log("No output on screen when both message and prepend string are empty");
+// console.log("Prints prepend string when message is empty");
+//
+// console.log("Prints complete message string with prepend when message is not empty");
+//
+// console.log("No output on screen when both message and prepend string are empty");
 module.exports = logger;
