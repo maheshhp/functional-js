3944be59c91e8b682c60a11e8a7cab09ea4aa90e
maheshhp <hpmahesh73@gmail.com>
2018-01-17 14:33:14 +0530

    Exercise eight: Fixed the function for exercise verification

--- duck-count.js
+++ duck-count.js
@@ -1,8 +1,9 @@
 function duckCount() {
   // SOLUTION GOES HERE
   let args = [].slice.call(arguments);
+  // console.log(args);
   let count = args.reduce(function (dckCount, obj) {
-    if (obj.hasOwnProperty("quack")) {
+    if (({}).hasOwnProperty.call(obj, "quack")) {
       dckCount+=1;
     }
     return dckCount;
@@ -11,8 +12,8 @@ function duckCount() {
 }
 
 //Testing the count function
-console.log("Passing Object with 0 quack functions, return 0 : ", duckCount({hello: 1}) === 0);
-console.log("Passing Object with 1 quack function, return 1 : ", duckCount({hello: 1}, {quack: 10, hello: 30}) === 1);
-console.log("Passing Object with 1 Quack function, return 0 : ", duckCount({hello: 1}, {Quack: 10, hello: 30}) === 0);
-console.log("Passing Object with multiple quack functions, returns correct count : ", duckCount({hello: 1, quack: 10}, {quack: 10, hello: 30}) === 2);
+// console.log("Passing Object with 0 quack functions, return 0 : ", duckCount({hello: 1}) === 0);
+// console.log("Passing Object with 1 quack function, return 1 : ", duckCount({hello: 1}, {quack: 10, hello: 30}) === 1);
+// console.log("Passing Object with 1 Quack function, return 0 : ", duckCount({hello: 1}, {Quack: 10, hello: 30}) === 0);
+// console.log("Passing Object with multiple quack functions, returns correct count : ", duckCount({hello: 1, quack: 10}, {quack: 10, hello: 30}) === 2);
 module.exports = duckCount;
