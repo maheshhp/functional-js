2129f84cf20e5117d05925160eb373618c75262b
maheshhp <hpmahesh73@gmail.com>
2018-01-17 14:27:41 +0530

    Exercise eight: Return value for multiple quack objects test passing

--- duck-count.js
+++ duck-count.js
@@ -14,4 +14,5 @@ function duckCount() {
 console.log("Passing Object with 0 quack functions, return 0 : ", duckCount({hello: 1}) === 0);
 console.log("Passing Object with 1 quack function, return 1 : ", duckCount({hello: 1}, {quack: 10, hello: 30}) === 1);
 console.log("Passing Object with 1 Quack function, return 0 : ", duckCount({hello: 1}, {Quack: 10, hello: 30}) === 0);
+console.log("Passing Object with multiple quack functions, returns correct count : ", duckCount({hello: 1, quack: 10}, {quack: 10, hello: 30}) === 2);
 module.exports = duckCount;
