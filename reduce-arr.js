7436b68d6d3cc84417a003befc6707ec828131c5
maheshhp <hpmahesh73@gmail.com>
2018-01-17 10:40:08 +0530

    Exercise Six: Example object return test passing

--- reduce-arr.js
+++ reduce-arr.js
@@ -3,9 +3,19 @@ function countWords(inputWords) {
   if (inputWords.length === 0) {
     return [];
   }
+  return inputWords.reduce(function (allFruits, fruit) {
+    if (fruit in allFruits) {
+      allFruits[fruit]++;
+    }
+    else {
+      allFruits[fruit] = 1;
+    }
+    return allFruits;
+  }, {});
 }
 
 let inputWords = ["Apple", "Banana", "Apple", "Durian", "Durian", "Durian"];
 let noInputWords = [];
-console.log("Return undefined for zero objects : ", countWords(noInputWords).length === 0);
+console.log("Return [] for zero objects : ", countWords(noInputWords).length === 0);
+console.log("Return correct count values : ", countWords(inputWords).Apple === 2 && countWords(inputWords).Durian === 3 && countWords(inputWords).Banana === 1);
 module.exports = countWords;
