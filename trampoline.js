b5fcfb08e228bd114e12cb69988a7c0e5ac3ac28
maheshhp <hpmahesh73@gmail.com>
2018-01-19 14:32:15 +0530

    Exercise fourteen: Verified the exercise

---
+++ trampoline.js
@@ -0,0 +1,25 @@
+function repeat(operation, num) {
+  // Modify this so it doesn't cause a stack overflow!
+  if (num <= 0) return;
+  operation();
+  setTimeout(function () {
+    return repeat(operation, --num);
+  }, 1);
+  // return repeat(operation, --num);
+}
+
+function trampoline(fn) {
+  // You probably want to implement a trampoline!
+  while (fn != null && fn != undefined && typeof (fn === "function")) {
+    let returnedFn = fn();
+    fn = returnedFn;
+  }
+}
+
+module.exports = function(operation, num) {
+  // You probably want to call your trampoline here!
+  trampoline(function () {
+    repeat(operation, num);
+  });
+  // return repeat(operation, num);
+};
